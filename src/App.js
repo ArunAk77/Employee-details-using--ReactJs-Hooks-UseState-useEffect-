788d0d6a39ec1cd91ee3ebd2354b46641b21c805
ArunAK77 <infantarun11@gmail.com>
2024-08-22 21:00:15 +0530

    Employee Details

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import ShoppingCart from './ShoppingCart';
 
 const App = () => {
   const sampleProducts = [
-    { id: 1, name: 'Product A', price: 10 },
-    { id: 2, name: 'Product B', price: 20 },
-    { id: 3, name: 'Product C', price: 30 },
+    { id: 1, name: 'Tennis Ball', price: 40 },
+    { id: 2, name: 'Cricket Bat', price: 20 },
+    { id: 3, name: 'Stumps', price: 30 },
   ];
 
   return (
